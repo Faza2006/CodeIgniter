b7f58c955e47492b703acd6a8fd2a37fbc6adb1d
Gestananda <fazzgestananda@gmail.com>
2022-12-04 23:37:34 +0700

    PHP SMK 20

---
+++ bag-20/ci4/app/Views/kategori/select.php
@@ -0,0 +1,54 @@
+<?= $this->extend('template/admin') ?>
+
+
+
+<?= $this->section('content') ?>
+
+<div class="row">
+
+<div class="col-4">
+    <a class="btn btn-outline-primary" href="<?= base_url()  ?>/admin/kategori/create" role="button">Tambah Data</a>
+</div>
+
+<div class="col-4">
+    <h3> <?= $judul; ?> </h3>
+</div>
+
+</div>
+
+
+
+
+
+
+<div class="row mt-2">
+
+<table class="table">
+
+    <tr>
+        <th>No</th>
+        <th>Kategori</th>
+        <th>Keterangan</th>
+        <th>Hapus</th>
+        <th>Ubah</th>
+    </tr>
+    <?php $no=1 ?>
+    <?php foreach($kategori as $key => $value): ?>
+    <tr>
+        <td><?= $no++ ?></td>
+        <td><?= $value['kategori'] ?></td>
+        <td><?= $value['keterangan'] ?></td>
+        <td><a href="<?=base_url() ?>/admin/kategori/delete/<?= $value['idkategori'] ?>">Hapus</a></td>
+        <td><a href="<?=base_url() ?>/admin/kategori/find/<?= $value['idkategori'] ?>">Ubah</a></td>
+
+    </tr>
+    <?php endforeach; ?>
+
+</table>
+
+<?= $pager->links('group1','bootstrap')?>
+
+</div>
+
+
+<?= $this->endSection() ?>
