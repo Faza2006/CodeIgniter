07063869de469221eece368d3319cd78b7e18b33
Gestananda <fazzgestananda@gmail.com>
2022-12-04 23:03:44 +0700

    CodeIgniter Bag 5

---
+++ bag-5/ci4/app/Controllers/Admin/kategori.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+
+class kategori extends BaseController
+{
+    public function index()
+    {
+        // return view('welcome_message');
+        echo "<h1>belajar ci4</h1>";
+    }
+
+    public function select()
+    {
+        echo "<h1>menampilkan semua data</h1>";
+    }
+
+    public function selectWhere($id = null)
+    {
+        echo "<h1>menampilkan data yang dipilih $id</h1>";
+    }
+
+    public function formInsert()
+    {
+        echo "<h1>menampilkan form insert</h1>";
+    }
+
+    public function formUpdate($id=null)
+    {
+        echo "<h1>menampilkan form update $id </h1>";
+    }
+
+    public function update($id = null)
+    {
+        echo "proses update data $id";
+    }
+
+    public function delete($id = null)
+    {
+        echo "proses delete data";
+    }
+
+    //----------------------------------------------
+
+}
